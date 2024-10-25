d7d2d6d4d6d243b8dc45fbe1e05a68ff9de71564
Breeze-P <noreply@greptime.com>
2024-10-25 20:04:44 +0800

    feat: support macos by sharing memory on oridinary file

--- src/memfd.rs
+++ src/memfd.rs
@@ -1,8 +1,8 @@
-use std::ffi::CString;
 use std::fs;
 use std::os::fd::FromRawFd;
 use std::os::fd::IntoRawFd;
 
+#[cfg(not(target_os = "macos"))]
 use nix::sys::memfd;
 use snafu::ResultExt;
 
@@ -20,21 +20,59 @@ pub struct MemfdSettings {
 
 /// Create a memfd with the given settings.
 pub fn memfd_create(settings: MemfdSettings) -> Result<fs::File> {
-    let MemfdSettings { name, size } = settings;
+    #[cfg(not(target_os = "macos"))]
+    {
+        use std::ffi::CString;
+        let MemfdSettings { name, size } = settings;
 
-    let c_name = CString::new(name.clone()).context(error::NulZeroSnafu)?;
+        let c_name = CString::new(name.clone()).context(error::NulZeroSnafu)?;
+        // TODO: add sealing support. This can prevent some permission issues,
+        // such as a user without write permissions writing to the ringbuf.
+        let flags = memfd::MemFdCreateFlag::MFD_CLOEXEC;
 
-    let flags = memfd::MemFdCreateFlag::MFD_CLOEXEC;
+        let owned_fd = memfd::memfd_create(&c_name, flags)
+            .context(error::MemFdSnafu { fd_name: name })?;
 
-    let owned_fd = memfd::memfd_create(&c_name, flags)
-        .context(error::MemFdSnafu { fd_name: name })?;
+        let raw_fd = owned_fd.into_raw_fd();
 
-    let raw_fd = owned_fd.into_raw_fd();
+        let file = unsafe { fs::File::from_raw_fd(raw_fd) };
+        file.set_len(size).context(error::IoSnafu)?;
 
-    let file = unsafe { fs::File::from_raw_fd(raw_fd) };
-    file.set_len(size).context(error::IoSnafu)?;
+        Ok(file)
+    }
+    #[cfg(target_os = "macos")]
+    {
+        let MemfdSettings { name, size } = settings;
 
-    Ok(file)
+        let base_path = String::from("/tmp/shm/");
+        let path = base_path + &name;
+        // Checking if the directory exists and creating if not
+        if let Some(parent_dir) = std::path::Path::new(&path).parent() {
+            if !parent_dir.exists() {
+                fs::create_dir_all(parent_dir).context(error::IoSnafu)?;
+            }
+        };
+
+        let owned_fd = fs::OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .truncate(true)
+            .open(&path)
+            .context(error::IoSnafu)?;
+
+        // In order to reduce the writing to the disk, we can delete the file immediately, why?
+        // Refer to: https://stackoverflow.com/questions/39779517/does-mac-os-have-a-way-to-create-an-anonymous-file-mapping
+        // But we are only developing on macOS and will not use macOS as a production environment. So we will comment out the following.
+        // fs::remove_file(path).context(error::IoSnafu)?;
+
+        let raw_fd = owned_fd.into_raw_fd();
+
+        let file = unsafe { fs::File::from_raw_fd(raw_fd) };
+        file.set_len(size).context(error::IoSnafu)?;
+
+        Ok(file)
+    }
 }
 
 #[cfg(test)]
