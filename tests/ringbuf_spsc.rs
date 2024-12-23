97c492d5c253e13e053508b72d17c2a06cfba9e8
fys <fengys1996@gmail.com>
2024-12-23 17:10:24 +0800

    chore: add test_ringbuf_spsc_with_notify

--- tests/ringbuf_spsc.rs
+++ tests/ringbuf_spsc.rs
@@ -1,17 +1,13 @@
-use std::str::from_utf8;
+mod common;
+
 use std::sync::Arc;
 use std::time::Duration;
 
-use shm_ringbuf::consumer::process::DataProcess;
+use common::{msg_num, reserve_with_retry, wait_consumer_online, MsgForward};
 use shm_ringbuf::consumer::settings::ConsumerSettingsBuilder;
 use shm_ringbuf::consumer::RingbufConsumer;
-use shm_ringbuf::error::DataProcessResult;
-use shm_ringbuf::error::{self};
-use shm_ringbuf::producer::prealloc::PreAlloc;
 use shm_ringbuf::producer::settings::ProducerSettingsBuilder;
 use shm_ringbuf::producer::RingbufProducer;
-use tokio::sync::mpsc::Sender;
-use tokio::time::sleep;
 
 #[tokio::test]
 async fn test_ringbuf_spsc() {
@@ -30,7 +26,7 @@ async fn test_ringbuf_spsc() {
         .build();
 
     tokio::spawn(async move {
-        let string_print = StringPrint { sender: send_msgs };
+        let string_print = MsgForward { sender: send_msgs };
         RingbufConsumer::new(settings).run(string_print).await;
     });
 
@@ -45,30 +41,26 @@ async fn test_ringbuf_spsc() {
     let producer =
         Arc::new(RingbufProducer::connect_lazy(settings).await.unwrap());
 
-    let msg_num = 100;
-    let mut joins = Vec::with_capacity(msg_num);
+    let msg_num = msg_num();
 
-    for i in 0..msg_num {
-        let mut pre_alloc =
-            reserve_with_retry(&producer, 20, 3, Duration::from_secs(1))
+    tokio::spawn(async move {
+        for i in 0..msg_num {
+            let mut pre_alloc =
+                reserve_with_retry(&producer, 20, 3, Duration::from_secs(1))
+                    .await
+                    .unwrap();
+
+            let write_str = format!("hello, {}", i);
+
+            wait_consumer_online(&producer, 5, Duration::from_secs(3))
                 .await
                 .unwrap();
 
-        let write_str = format!("hello, {}", i);
+            pre_alloc.write(write_str.as_bytes()).unwrap();
 
-        wait_consumer_online(&producer, 5, Duration::from_secs(3))
-            .await
-            .unwrap();
-
-        pre_alloc.write(write_str.as_bytes()).unwrap();
-
-        pre_alloc.commit();
-
-        joins.push(pre_alloc.wait_result());
-    }
-    for j in joins {
-        let _ = j.await;
-    }
+            pre_alloc.commit();
+        }
+    });
 
     for i in 0..msg_num {
         let msg = format!("hello, {}", i);
@@ -76,86 +68,65 @@ async fn test_ringbuf_spsc() {
     }
 }
 
-async fn reserve_with_retry(
-    producer: &RingbufProducer,
-    size: usize,
-    retry_num: usize,
-    retry_interval: Duration,
-) -> Result<PreAlloc, String> {
-    for _ in 0..retry_num {
-        let err = match producer.reserve(size) {
-            Ok(pre) => return Ok(pre),
-            Err(e) => e,
-        };
+#[tokio::test]
+async fn test_ringbuf_spsc_with_notify() {
+    tracing_subscriber::fmt::init();
 
-        if !matches!(err, error::Error::NotEnoughSpace { .. }) {
-            break;
+    let (send_msgs, mut recv_msgs) = tokio::sync::mpsc::channel(100);
+
+    let dir = tempfile::tempdir().unwrap();
+    let grpc_sock_path = dir.path().join("control.sock");
+    let fdpass_sock_path = dir.path().join("sendfd.sock");
+
+    let settings = ConsumerSettingsBuilder::new()
+        .grpc_sock_path(grpc_sock_path.clone())
+        .fdpass_sock_path(fdpass_sock_path.clone())
+        .process_interval(Duration::from_millis(10))
+        // Set too long interval for testing notify.
+        .process_interval(Duration::from_millis(1000))
+        .build();
+
+    tokio::spawn(async move {
+        let string_print = MsgForward { sender: send_msgs };
+        RingbufConsumer::new(settings).run(string_print).await;
+    });
+
+    // Wait for the consumer to start.
+    tokio::time::sleep(Duration::from_millis(10)).await;
+
+    let settings = ProducerSettingsBuilder::new()
+        .grpc_sock_path(grpc_sock_path.clone())
+        .fdpass_sock_path(fdpass_sock_path.clone())
+        .build();
+
+    let producer =
+        Arc::new(RingbufProducer::connect_lazy(settings).await.unwrap());
+
+    let msg_num = msg_num();
+
+    tokio::spawn(async move {
+        for i in 0..msg_num {
+            let mut pre_alloc =
+                reserve_with_retry(&producer, 20, 3, Duration::from_secs(1))
+                    .await
+                    .unwrap();
+
+            let write_str = format!("hello, {}", i);
+
+            wait_consumer_online(&producer, 5, Duration::from_secs(3))
+                .await
+                .unwrap();
+
+            pre_alloc.write(write_str.as_bytes()).unwrap();
+
+            pre_alloc.commit();
+
+            producer.notify_consumer(Some(1000)).await;
         }
-        sleep(retry_interval).await;
-    }
+    });
 
-    Err("reserve failed".to_string())
-}
-
-async fn wait_consumer_online(
-    p: &RingbufProducer,
-    retry_num: usize,
-    retry_interval: Duration,
-) -> Result<(), String> {
-    for _ in 0..retry_num {
-        if p.server_online() && p.result_fetch_normal() {
-            return Ok(());
-        }
-
-        sleep(retry_interval).await;
-    }
-
-    Err("wait consumer online timeout".to_string())
-}
-
-pub struct StringPrint {
-    sender: Sender<String>,
-}
-
-impl DataProcess for StringPrint {
-    type Error = Error;
-
-    async fn process(&self, data: &[u8]) -> Result<(), Self::Error> {
-        let msg = from_utf8(data).map_err(|_| Error::DecodeError)?;
-
-        let _ = self.sender.send(msg.to_string()).await;
-
-        Ok(())
-    }
-}
-
-#[derive(Debug)]
-pub enum Error {
-    DecodeError,
-    ProcessError,
-}
-
-impl Error {
-    pub fn status_code(&self) -> u32 {
-        match self {
-            Error::DecodeError => 1001,
-            Error::ProcessError => 1002,
-        }
-    }
-
-    pub fn message(&self) -> String {
-        match self {
-            Error::DecodeError => "decode error".to_string(),
-            Error::ProcessError => "process error".to_string(),
-        }
-    }
-}
-
-impl From<Error> for DataProcessResult {
-    fn from(err: Error) -> DataProcessResult {
-        DataProcessResult {
-            status_code: err.status_code(),
-            message: err.message(),
-        }
+    for i in 0..msg_num {
+        let msg = format!("hello, {}", i);
+        assert_eq!(recv_msgs.recv().await.unwrap(), msg);
     }
 }
